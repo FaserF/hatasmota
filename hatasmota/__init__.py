4b9c7ef207936895654acaf7f99235192e91812d
Fabian Seitz <fabian.seitz@pari.com>
2026-04-10 12:27:07 +0200

    add missing import

--- hatasmota/__init__.py
+++ hatasmota/__init__.py
@@ -1,10 +1,11 @@
 """HATasmota."""
 
 from .const import COMMAND_UPGRADE
-from .update import TasmotaUpdate, TasmotaUpdateConfig
+from .update import TasmotaUpdate, TasmotaUpdateConfig, is_stock_build
 
 __all__ = [
     "COMMAND_UPGRADE",
     "TasmotaUpdate",
     "TasmotaUpdateConfig",
+    "is_stock_build",
 ]
